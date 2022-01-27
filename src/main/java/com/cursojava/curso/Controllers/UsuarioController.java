488f6553ffb1d377ccf2d21b03870f028a5501b1
Javier cuasapud <edwarcuasapud@gmail.com>
2022-01-27 14:20:37 -0500

    segundo commit del proyecto se agrego la parete visible

--- src/main/java/com/cursojava/curso/Controllers/UsuarioController.java
+++ src/main/java/com/cursojava/curso/Controllers/UsuarioController.java
@@ -1,12 +1,22 @@
 package com.cursojava.curso.Controllers;
+import com.cursojava.curso.Controllers.models.Usuario;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class UsuarioController {
-    @RequestMapping(value = "prueba")
-    public String prueba() {
-        return "agua de coco";
+    @RequestMapping(value = "usuario")
+    public Usuario getUsuario() {
+        Usuario usuario = new Usuario();
+        usuario.setNombre("javier");
+        usuario.setApellido("cuasapud");
+        usuario.setEmail("cuasa@gmail.com");
+        usuario.setTelefono("121221");
+        usuario.setPassword("ssddsd");
+        return  usuario;
     }
+
+
 }
